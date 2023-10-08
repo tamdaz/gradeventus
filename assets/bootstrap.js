958bdd911d83db4d2857bed7239ccf375197a810
tamdaz <tamdazohir01@gmail.com>
2023-10-08 13:50:00 +0200

    Add dark mode and app icon

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,8 +1,7 @@
-import { startStimulusApp } from '@symfony/stimulus-bridge';
-import LiveController from '@symfony/ux-live-component';
 import '@symfony/ux-live-component/styles/live.css';
+import LiveController from '@symfony/ux-live-component';
+import { startStimulusApp } from '@symfony/stimulus-bridge';
 
-// Registers Stimulus controllers from controllers.json and in the controllers/ directory
 export const app = startStimulusApp(require.context(
     '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
     true,
